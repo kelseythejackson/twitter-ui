48807a8942ede2e32d26ff3e87840761a9794f0f
Kelsey Jackson <kelseythej@gmail.com>
2017-12-24 18:56:58 -0600

    finished most steps

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
 const express = require('express');
 const app = express();
+const t = require('./config');
+app.use(express.static('public'));
+app.set('view engine', 'pug');
+app.locals.moment = require('moment');
 
-app.get('/',(req, res) => {
-    res.send('Working');
+app.use((req, res, next) => {
+    t.get('account/verify_credentials', { name: 'Kelsey Jackson'}, function (err, creds, res) {
+        req.creds = creds;
+        next(); 
+    });
 });
+
+app.use((req, res, next) => {
+    t.get('statuses/user_timeline', { screen_name: 'paisley_darts', count: 5 }, function (err, tweets, res) {
+        req.tweets = tweets;
+        console.log(tweets[0]);
+        next(); 
+    });
+});
+
+app.use((req, res, next) => {
+    t.get('friends/list', { screen_name: 'paisley_darts', count: 10 }, function (err, list, res) {
+        req.list = list.users;
+        next(); 
+    });
+});
+
+app.use((req, res, next) => {
+    t.get('direct_messages/sent', { count: 5 }, function (err, messages, res) {
+        req.messages = messages;
+        
+        next(); 
+    });
+});
+
+app.use('/',(req, res) => {
+    const { creds } = req;
+    const { tweets } = req;
+    const { list }  = req;
+    const { messages } = req;
+    res.render('index', { creds, tweets, list, messages });
+}); 
+
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
