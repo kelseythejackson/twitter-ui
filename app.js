9c4fb25a373bb7cbce3da5973d4041435b9ee64c
Kelsey Jackson <kelseythej@gmail.com>
2017-12-30 22:34:33 -0600

    Clean up

--- app.js
+++ app.js
@@ -1,72 +1,45 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const t = require('./config');
+
 app.use(express.static('public'));
 app.set('view engine', 'pug');
-app.locals.moment = require('moment');
-
-app.use(bodyParser.urlencoded({ extended: false }));
 
 
+
+app.use(
+    (req, res, next) => {
+        t.get('statuses/user_timeline', { screen_name: 'paisley_darts', count: 5, include_rts: true }, function (err, data, res) {
+           
+            req.tweets = data;
+            next();
+        });
+    },
+    (req, res, next) => {
+        t.get('friends/list', { screen_name: 'paisley_darts', count: 15 }, function (err, list, res) {
+            req.friendsList = list.users;
+            next(); 
+        });  
+    },
+    (req, res, next) => {
+        t.get('direct_messages/sent', { count: 5 }, function (err, messages, res) {
+            req.messages = messages;
+            next(); 
+        });  
+    }
+);
+
+app.get('/', (req, res) => {
+    const { tweets, friendsList, messages } = req;
     
-
-
-app.use((req, res, next) => {
-    t.get('account/verify_credentials', { name: 'Kelsey Jackson'}, function (err, creds, res) {
-        req.creds = creds;
-        console.log(req.creds);
-        next(); 
-    });
-});
-
-app.use((req, res, next) => {
-    t.get('statuses/user_timeline', { screen_name: 'paisley_darts', count: 5 }, function (err, tweets, res) {
-        req.tweets = tweets;
-        // console.log(tweets[0]);
-        next(); 
-    });
-});
-
-app.use((req, res, next) => {
-    t.get('friends/list', { screen_name: 'paisley_darts', count: 15 }, function (err, list, res) {
-        req.list = list.users;
-        next(); 
-    });
-});
-
-app.use((req, res, next) => {
-    t.get('direct_messages/sent', { count: 5 }, function (err, messages, res) {
-        req.messages = messages;
-        next(); 
-    });
-});
-
-app.use((req, res, next) => {
-    const { post } = req.body;
-    req.post = post;
-    console.log(req.post);
-    next();
-});
-
-app.use('/',(req, res) => {
-    const { list }  = req;
-    const { creds } = req;
-    const { tweets } = req;
-    const { messages } = req;
-
-    res.render('index', { list, creds, tweets, messages });
-    next();
-}); 
-
-app.post('/', (req, res, next)=> {
-    const {post} = req;
-    res.render('index', post);
-});
-
-
-
-
+    
+    // console.log(tweets[0].retweeted_status.retweet_count);
+    // console.log(tweets[0].retweeted_status.favorite_count);
+    // console.log(tweets[0].retweeted_status);
+    console.log(tweets[0]);
+    
+    res.render('index', { tweets, friendsList, messages });
+})
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
 });
