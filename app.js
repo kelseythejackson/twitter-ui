6d361fe4b71551cff33de55325c88c3c19f2192d
Kelsey Jackson <kelseythej@gmail.com>
2018-01-01 19:46:19 -0600

    Finish endpoints

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 const express = require('express');
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 const t = require('./config');
+const moment = require('moment');
 
+app.locals.moment = moment;
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 
-
-
 app.use(
     (req, res, next) => {
-        t.get('statuses/user_timeline', { screen_name: 'paisley_darts', count: 5, include_rts: true }, function (err, data, res) {
-           
+        t.get('account/verify_credentials', { name: 'Kelsey Jackson'}, (err, creds, res) => {
+            req.creds = creds;
+            next(); 
+        });
+    },
+    (req, res, next) => {
+        t.get('statuses/user_timeline', { screen_name: 'paisley_darts', count: 5, include_rts: true }, (err, data, res) => {
             req.tweets = data;
             next();
         });
     },
     (req, res, next) => {
-        t.get('friends/list', { screen_name: 'paisley_darts', count: 15 }, function (err, list, res) {
+        t.get('friends/list', { screen_name: 'paisley_darts', count: 15 }, (err, list, res) => {
             req.friendsList = list.users;
             next(); 
         });  
     },
     (req, res, next) => {
-        t.get('direct_messages/sent', { count: 5 }, function (err, messages, res) {
+        t.get('direct_messages/sent', { count: 5 }, (err, messages, res) => {
             req.messages = messages;
             next(); 
         });  
@@ -30,16 +37,23 @@ app.use(
 );
 
 app.get('/', (req, res) => {
-    const { tweets, friendsList, messages } = req;
-    
-    
-    // console.log(tweets[0].retweeted_status.retweet_count);
-    // console.log(tweets[0].retweeted_status.favorite_count);
-    // console.log(tweets[0].retweeted_status);
-    console.log(tweets[0]);
-    
-    res.render('index', { tweets, friendsList, messages });
-})
-app.listen(3000, () => {
+    const { creds, tweets, friendsList, messages } = req;
+    res.render('index', { creds, tweets, friendsList, messages });
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function(data){   
+        t.post('statuses/update', { status: data }, function(err, data, response) {
+            const date = moment(data.created_at, 'ddd MMM DD HH:mm:ss ZZ YYYY').fromNow();
+            io.emit('chat message', {data, date});
+        });      
+    });    
+});
+server.listen(3000, () => {
     console.log('App listening on port 3000!');
 });
